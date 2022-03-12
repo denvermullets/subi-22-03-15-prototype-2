0c5f68fa6859ffc7d2169cc7066853c0e2e7f77c
denvermullets <devmullets@gmail.com>
2022-03-11 20:36:24 -0500

    add player collisions, add movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,28 @@
-extends Node
+extends KinematicBody2D
 
+var velocity: Vector2
+var speed: int = 200
+var direction: Vector2 = Vector2()
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func read_input() -> void:
+  velocity = Vector2()
 
+  if Input.is_action_pressed("key_up"):
+    velocity.y -= 1
+    direction = Vector2(0, -1)
+  if Input.is_action_pressed("key_down"):
+    velocity.y += 1
+    direction = Vector2(0, 1)
+  if Input.is_action_pressed("key_left"):
+    velocity.x -= 1
+    direction = Vector2(-1, 0)
+  if Input.is_action_pressed("key_right"):
+    velocity.x += 1
+    direction = Vector2(1, 0)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+  velocity = velocity.normalized()
+  velocity = move_and_slide(velocity * speed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	read_input()
